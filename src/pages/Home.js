5e02f93abfb542f3e4778b14deaf5147f8204666
Nitu Dubey <nitutiwari01@gmail.com>
2023-11-02 18:20:07 -0400

    added loading component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,7 @@ import SearchBar from "../components/common/SearchBar";
 import Pagination from "../components/common/Pagination";
 import Header from "../components/common/Header";
 import Footer from "../components/common/Footer";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const Home = () => {
   const productState = useSelector((state) => state.product); //to read the store data
@@ -34,95 +35,104 @@ const Home = () => {
   return (
     <div className="home">
       <Header />
-      <SearchBar filterProducts={filterProducts} />
-
-      {/* <!-- buttons --using bootstrap--> */}
-      <div className="buttons mb-5 pt-4 ">
-        <button
-          id="btn-all"
-          className="btn btn-filter  p-2"
-          style={{
-            width: "15%",
-            height: "5vh",
-            "marginRight": "10px",
-            "borderRadius": "0.5rem",
-          }}
-          onClick={() => {
-            filterProducts();
-          }}
-        >
-          All
-        </button>
-        <button
-          id="btn-men"
-          className="btn btn-filter p-2"
-          style={{
-            width: "15%",
-            height: "5vh",
-            "marginRight": "10px",
-            "borderRadius": "0.5rem",
-          }}
-          onClick={() => {
-            filterProducts("men's clothing");
-          }}
-        >
-          Mens
-        </button>
-        <button
-          id="btn-women"
-          className="btn btn-filter p-2"
-          style={{
-            width: "15%",
-            height: "5vh",
-            "marginRight": "10px",
-            "borderRadius": "0.5rem",
-          }}
-          onClick={() => {
-            filterProducts("women's clothing");
-          }}
-        >
-          Womens
-        </button>
-        <button
-          id="btn-jewellery"
-          className="btn btn-filter  p-2"
-          style={{
-            width: "15%",
-            height: "5vh",
-            "marginRight": "10px",
-            "borderRadius": "0.5rem",
-          }}
-          onClick={() => {
-            filterProducts("jewelery");
-          }}
-        >
-          Jewllery
-        </button>
-        <button
-          id="btn-electronics"
-          className="btn  btn-filter p-2"
-          style={{ width: "15%", height: "5vh", "borderRadius": "0.5rem" }}
-          onClick={() => {
-            filterProducts("electronics");
-          }}
-        >
-          Electronics
-        </button>
-      </div>
-
-      {productState.products.length > 0 ? (
-        <Cards state={productState} />
+      {productState.loading ? (
+        <div className="loading">
+          <CircularProgress color="secondary" />
+        </div>
       ) : (
-        <div className="noProducts">No products matching the filter</div>
+        <>
+          <SearchBar filterProducts={filterProducts} />
+
+          {/* <!-- buttons --using bootstrap--> */}
+          <div className="buttons mb-5 pt-4 ">
+            <button
+              id="btn-all"
+              className="btn btn-filter  p-2"
+              style={{
+                width: "15%",
+                height: "5vh",
+                marginRight: "10px",
+                borderRadius: "0.5rem",
+              }}
+              onClick={() => {
+                filterProducts();
+              }}
+            >
+              All
+            </button>
+            <button
+              id="btn-men"
+              className="btn btn-filter p-2"
+              style={{
+                width: "15%",
+                height: "5vh",
+                marginRight: "10px",
+                borderRadius: "0.5rem",
+              }}
+              onClick={() => {
+                filterProducts("men's clothing");
+              }}
+            >
+              Mens
+            </button>
+            <button
+              id="btn-women"
+              className="btn btn-filter p-2"
+              style={{
+                width: "15%",
+                height: "5vh",
+                marginRight: "10px",
+                borderRadius: "0.5rem",
+              }}
+              onClick={() => {
+                filterProducts("women's clothing");
+              }}
+            >
+              Womens
+            </button>
+            <button
+              id="btn-jewellery"
+              className="btn btn-filter  p-2"
+              style={{
+                width: "15%",
+                height: "5vh",
+                marginRight: "10px",
+                borderRadius: "0.5rem",
+              }}
+              onClick={() => {
+                filterProducts("jewelery");
+              }}
+            >
+              Jewllery
+            </button>
+            <button
+              id="btn-electronics"
+              className="btn  btn-filter p-2"
+              style={{ width: "15%", height: "5vh", borderRadius: "0.5rem" }}
+              onClick={() => {
+                filterProducts("electronics");
+              }}
+            >
+              Electronics
+            </button>
+          </div>
+
+          {productState.products.length > 0 ? (
+            <Cards state={productState} />
+          ) : (
+            <div className="noProducts">No products matching the filter</div>
+          )}
+
+          {productState.products.length > 0 ? (
+            <Pagination
+              currentPage={productState.currentPage}
+              totalPages={productState.totalPages}
+              onPageChange={handlePageChange}
+            />
+          ) : null}
+        </>
       )}
 
-      {productState.products.length >0 ? (
-        <Pagination
-          currentPage={productState.currentPage}
-          totalPages={productState.totalPages}
-          onPageChange={handlePageChange}
-        />
-      ) : null}
       <Footer />
     </div>
   );
